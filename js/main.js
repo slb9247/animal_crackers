613d3c073c6de53e883661c11597c2da7b01662b
Samantha Bolotine <slb1992@yahoo.com>
2016-09-30 22:56:46 -0400

    navigation

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).on('ready', function() {
+
+  /* hamburger menu */
+    $('.hamburger, .menu-overlay').on('click', function() {
+      $('.hamburger').toggleClass('is-active');
+      $('.menu-overlay').fadeToggle(200);
+      $('body').toggleClass('hide');
+    });
+
+
+
+    // if($('.hamburger').css('display') == 'none') {
+    //   $('.menu-overlay').fadeOut('slow');
+    // }
+
+
+
+}); // end doc.ready
