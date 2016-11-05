32c3c9920786f8b8219d155fb92e9da5873cd4f2
Samantha Bolotine <slb1992@yahoo.com>
2016-11-05 14:53:55 -0400

    slideshow

--- js/main.js
+++ js/main.js
@@ -20,6 +20,56 @@ $(document).on('ready', function() {
     });
 
 
+  /* grooming page slideshow */
+
+    // // show first image by default
+    // $('#slides img').eq(0).show();
+    // var currentSlide = 0;
+    // var slideCount = $('#slides img').length;
+    //
+    // var slideBtn = function(slideBtn) {
+    //   // hide current slide
+    //   $('#slides img').eq(currentSlide).hide();
+    //
+    //   if (slideBtn === 'prev') {
+    //     // decrease currentSlide by 1
+    //     currentSlide = currentSlide - 1;
+    //
+    //     // if currentSlide is the first slide
+    //     if (currentSlide < 0) {
+    //       // update currentSlide to the last slide and decrease by 1
+    //       currentSlide = slideCount - 1; // length of array - 1
+    //     }
+    //   } else if (slideBtn === 'next') {
+    //       // increase currentSlide by 1
+    //       currentSlide = currentSlide + 1;
+    //
+    //       // if currentSlide is the last slide
+    //       if (currentSlide === slideCount) {
+    //         currentSlide = 0;
+    //       }
+    //     }
+    //   // update currentSlide BEFORE showing next slide
+    //   $('#slides img').eq(currentSlide).show();
+    // } /* end var slideBtn */
+    //
+    //
+    // // attach click event to prev button
+    // $('#btn-prev').on('click', function() {
+    //   slideBtn('prev');
+    // });
+    //
+    // // attach click event to next button
+    // $('#btn-next').on('click', function() {
+    //   slideBtn('next');
+    // });
+
+
+  $(function() {
+    $(".rslides").responsiveSlides();
+  });
+
+
 
     // if($('.hamburger').css('display') == 'none') {
     //   $('.menu-overlay').fadeOut('slow');
